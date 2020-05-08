3ee3d7c8d2325c5245d303bbccd354c5b1e22d44
Lais <laispetrav@gmail.com>
2020-05-08 14:13:30 -0300

    exercicio de objetos - ex 2

---
+++ semana4/lista-de-exercicios/objetos2.js
@@ -0,0 +1,12 @@
+
+let criaRetangulo = (lado1, lado2) => {
+    console.log("Altura:" + lado1)
+    console.log("Largura:" + lado2)
+    let area = lado1 * lado2
+    console.log("Area:" + area)
+    let perimetro = (lado1 + lado2)*2
+    console.log("Perimetro:" + perimetro)
+}
+
+console.log(criaRetangulo(2, 4))
+    
