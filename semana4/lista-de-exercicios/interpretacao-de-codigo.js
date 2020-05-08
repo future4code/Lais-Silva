80ca208e39b66129e775df19ec8e49a6452e6aa8
Lais <laispetrav@gmail.com>
2020-05-08 10:34:26 -0300

    exercicios de interpretação de codigo

---
+++ semana4/lista-de-exercicios/interpretacao-de-codigo.js
@@ -0,0 +1,32 @@
+/*
+EXERCICIO 1
+
+Função de conversão de moedas; 
+A variável cotação recebe uma resposta do usuário;
+O valor de cotação é então multiplicado pelo parametro incluido na invocação da função;
+É impresso no console o resultado da multiplicação entre os valores recebidos pela variável cotação e meuDinheiro.
+
+EXERCICIO 2
+
+Função para calculo de rendimento de aplicações;
+A variável valoraposInvestimento recebe um valor dependendo dos valores inseridos na invocação da função. 
+Existem condicionais para diferentes possibilidades de tipoDeInvestimento. 
+Caso a string correspondente ao tipoDeInvestimento tenha correspondencia no switch, o valor inserido também na incocação da função
+é multiplicado pelo indice de rendimento especifico. 
+Caso a string inserida na invocação nao tenha correspondencia, um alerta sera exibido na tela (caso do console.log(segundoMontante)).
+Em resposta ao console.log(novoMontante) sera impresso no console o number 165.
+
+EXERCICIO 3
+
+O código faz com que seja inserido na array1 e array2, respectivamente, 
+os numeros pares e impares dentre os numeros elencados na array numeros.
+O primeiro comando de console.log imprimirá uma string + o cumprimento da array numeros = Quantidade total de numeros 14.
+O segundo e terceiro comandos console.log farão imprimir o tamanho da array1 e array2.
+
+EXERCICIO 4
+
+O for aparece para encontrar o menor e o maior número da array. 
+Inicialmente os numeros são comparados com as variáveis numero1 e numero2,
+depois os numeros da array passam a ser comparados entre eles. 
+O comando console.log(numero1)imprimirá o menor numero da array numeros: -10.
+O comando console.log(numero1) imprimirá o maior numero da array numeros: 1590.
