9b797dd18ef3c27a9a8083af3d6538969f437c16
Lais <laispetrav@gmail.com>
2020-05-08 14:28:53 -0300

    exercicio de objetos - ex 3

---
+++ semana4/lista-de-exercicios/objetos3.js
@@ -0,0 +1,11 @@
+
+var filmePreferido = {
+    nome: 'Le Tout Nouveau Testament',
+    ano: '2016',
+    diretor: 'Jaco Van Dormael',
+    atores: 'Pili Groyne, Yolande Moreau, Benoît Poelvoorde'
+}
+
+console.log("Venha assistir ao filme " + filmePreferido.nome + ", de " + filmePreferido.ano
++ ", dirigido por " + filmePreferido.diretor + "e estrelado por " +
+filmePreferido.atores)
