b39bc6c46df5cac321c6e6618a2415510a0898ca
Lais <laispetrav@gmail.com>
2020-07-23 09:42:31 -0300

    atualização

--- semana4/funcoes-de-array/funcoes.js
+++ semana4/funcoes-de-array/funcoes.js
@@ -1,52 +1,8 @@
-let listaDeDespesas = []
+// exercicio1
 
-function gerarDespesa(event) {
-    valorDespesa = document.getElementById('valorDespesa')
-    tipoDespesa = document.getElementById('tipoDespesa')
-    extratoTotal = document.getElementById('extrato')
+const a = [1, 2, 3, 4]
+const reverse = (a) => {
+    const b = a.split('').reverse().join('')
     
-    let despesas = {
-    valor: valorDespesa.value,
-    tipo: tipoDespesa.value    
-    }
-  
-    if (valorDespesa.value === "" || tipoDespesa.value === ""){
-        alert("Nenhum campo pode estar vazio")
-    } else { 
-        listaDeDespesas.push(despesas) 
-            
-            let despesaOutros = listaDeDespesas.filter((despesa, index, array) => {
-            if (despesa.tipo === "outros"){
-            return true 
-            } return false
-            })
-
-            let despesaVinil = listaDeDespesas.filter((despesa, index, array) => {
-            if (despesa.tipo === "vinil"){
-            return true 
-            } return false
-            })
-
-            let despesaComida = listaDeDespesas.filter((despesa, index, array) => {
-            if (despesa.tipo === "comida"){
-            return true 
-            } return false
-            })
-        
-        valorDespesa.value = ""
-        tipoDespesa.value = ""
-    }
 }
-
-/*
-
-function valorTotal {
-    let total = 0
-    listaDeDespesas.forEach((valor, index, array) => {
-    let total = total += valor.valor
-    })
-    let totalEscrito = document.getElementById('extrato')
-    totalEscrito.innerHTML = `<p> R$${total} ,00`
-}
-
-*/
+console.log(b)
